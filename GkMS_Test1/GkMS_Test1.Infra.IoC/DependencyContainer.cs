973d409acfba9b1ed86b09a8bb01bd8675764945
sreejith_vikraman <sreejith.vikraman@efftabs.com>
2020-09-29 22:05:32 +0530

    Dependency Containers

---
+++ GkMS_Test1/GkMS_Test1.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using GkMS_Test1.Domain.Core.Bus;
+using GkMS_Test1.Infra.Bus;
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GkMS_Test1.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            });
+            
+        }
+    }
+}
