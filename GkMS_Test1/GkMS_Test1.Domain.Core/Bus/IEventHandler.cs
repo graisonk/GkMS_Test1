837c4e11c42163c04ef24d18267aa9baa544c91c
sreejith_vikraman <sreejith.vikraman@efftabs.com>
2020-09-29 21:01:22 +0530

    Implemented Domain.Core

---
+++ GkMS_Test1/GkMS_Test1.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using GkMS_Test1.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GkMS_Test1.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
